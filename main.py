ef0cae5e93ed32c8f8dd8934645a2de118f0fc39
aditya-10k <katheaditya10@gmail.com>
2025-03-24 01:27:13 +0530

    stock info , fundamentals , dividends , news done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
 from app import app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port=10000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host="0.0.0.0", port=port)
