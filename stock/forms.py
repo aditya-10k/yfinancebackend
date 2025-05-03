4b09c4eb94693b34d45fe12206b408a960422c6a
aditya-10k <katheaditya10@gmail.com>
2025-05-04 03:21:44 +0530

    ui of login , landing , signup done

--- stock/forms.py
+++ stock/forms.py
@@ -7,5 +7,18 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Remove help texts for cleaner UI
+        for field in self.fields.values():
+            field.help_text = ''
+
+        # Optional: Add placeholder support (matches HTML)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Enter username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Enter email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Create a password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm password'})
+
 class LoginForm(AuthenticationForm):
     pass
